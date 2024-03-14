ed45869837de775590c04145a808902ee176dac7
Madanmohan Tiwari <mmtiwari222@gmail.com>
2024-03-14 17:36:28 +0000

    All about variables

--- let.js
+++ let.js
@@ -1,9 +1,24 @@
+let StudentName = "Madanmohan Tiwari";
+console.log(StudentName); // Madanmohan Tiwari
+
 {
-    let StudentName = "Madanmohan Tiwari";
-    console.log(StudentName);
-}
+  {
+    // let StudentName = "Manmohan Tiwari"      // Cannot redeclare because let have block scope
     let StudentName = "Shiya Shukla";
-{
-    console.log(StudentName);
+    console.log(StudentName); // Shiya shukla
+  }
+
+  console.log(StudentName); // Madanmohan Tiwari
+
+  {
+    /*console.log(StudentName); 
+    ReferenceError: Cannot access 'StudentName' before initialization because
+    in same scope first we declare the varibale then we can use it  */
+
+    let StudentName = "Vivek Gupta";
+    console.log(StudentName); // Vivek Gupta
+  }
+
+  console.log(StudentName); // Madanmohan Tiwari
+  //let have block scope
 }
-  
---
+++ variables.js
@@ -0,0 +1,15 @@
+const StudentId = 1;
+let StudentName = "Madanmohan Tiwari";
+var StudentEmail = "mmt@gmail.com";
+var StudentEmail = "mm@gmail.com";
+let StudentLinkeldn;
+
+console.table([StudentId, StudentName, StudentEmail, StudentLinkeldn]);
+
+/* let var and const are keywords in JavaScript, which are use to declare variable in JS.
+let and const have block scope
+let and const can't be redeclared
+let and const must be declared before use
+let are const are not hoisted
+it is highly recommended now var is not use to declare variable because it have global scope
+Varable define with const cannot be reassigned */
